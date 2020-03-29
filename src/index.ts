9411a889679bc1c6d89daa2711fe4f76431792e7
Sorunome <mail@sorunome.de>
2020-03-29 10:31:21 +0200

    update dependency

--- src/index.ts
+++ src/index.ts
@@ -71,7 +71,7 @@ const puppet = new PuppetBridge(options["registration-file"], options.config, pr
 
 if (options.register) {
 	// okay, all we have to do is generate a registration file
-	puppet.readConfig();
+	puppet.readConfig(false);
 	try {
 		puppet.generateRegistration({
 			prefix: "_toxpuppet_",
